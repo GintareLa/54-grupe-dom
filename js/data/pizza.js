2f8c6e6409228bc1cd9014dc0489045758263ed7
Gintare <lapiene.g@gmail.com>
2025-04-08 14:46:43 +0300

    import

---
+++ js/data/pizza.js
@@ -0,0 +1,38 @@
+export const foodList = [
+    {
+        photo: 'Untitled.png',
+        name: 'Margarita',
+        price: 449,
+        availability: 4,
+    },
+    {
+         photo: 'Untitled.png',
+        name: 'Havaju',
+        price: 899,
+        availability: 18,
+    },
+    {
+         photo: 'Untitled.png',
+        name: 'Ham & Mushrooms',
+        price: 2100,
+        availability: 5,
+    },
+    {
+         photo: 'Untitled.png',
+        name: '4 sezonu',
+        price: 1299,
+        availability: 1,
+    },
+    {
+         photo: 'Untitled.png',
+        name: '4 suriu',
+        price: 1399,
+        availability: 1,
+    },
+    {
+         photo: 'Untitled.png',
+        name: 'Mafija',
+        price: 1199,
+        availability: 0,
+    },
+];
