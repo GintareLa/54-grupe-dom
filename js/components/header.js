a6cb0c3516edbd94f8977ae50abaaa0fb59de2de
Gintare <lapiene.g@gmail.com>
2025-04-09 17:22:31 +0300

    try5

--- js/components/header.js
+++ js/components/header.js
@@ -1,11 +1,11 @@
 export function header() {
     const menu = [
-        { text: 'Home', href: '/54-grupe-dom/' },
-        { text: 'Text', href: '/54-grupe-dom/text' },
-        { text: 'Food', href: '/54-grupe-dom/food' },
-        { text: 'Darzas', href: '/54-grupe-dom/darzas' },
-        { text: 'Header', href: '/54-grupe-dom/header' },
-        { text: 'Click', href: '/54-grupe-dom/click' },
+        { text: 'Home', href: '/' },
+        { text: 'Text', href: '/text' },
+        { text: 'Food', href: '/food' },
+        { text: 'Darzas', href: '/darzas' },
+        { text: 'Header', href: '/header' },
+        { text: 'Click', href: '/click' },
     ];
    
     const lp = location.pathname;
